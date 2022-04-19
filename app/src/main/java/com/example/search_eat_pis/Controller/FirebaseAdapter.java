1f6e4b518cc6a67268f21a158538a954a0467736
CarlosGD <cgomezdo22@alumnes.ub.edu>
2022-04-19 11:00:21 +0200

    Buscador

---
+++ app/src/main/java/com/example/search_eat_pis/Controller/FirebaseAdapter.java
@@ -0,0 +1,11 @@
+package com.example.search_eat_pis.Controller;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+
+public class FirebaseAdapter {
+    public static final String TAG = "DatabaseAdapter";
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+}
---
+++ app/src/main/java/com/example/search_eat_pis/Vista/MainMenu_Navegacion.java
@@ -0,0 +1,38 @@
+package com.example.search_eat_pis.Vista;
+
+import android.os.Bundle;
+
+import com.example.search_eat_pis.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.search_eat_pis.databinding.ActivityMainMenuNavegacionBinding;
+
+public class MainMenu_Navegacion extends AppCompatActivity {
+
+    private ActivityMainMenuNavegacionBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainMenuNavegacionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_menu_navegacion);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
