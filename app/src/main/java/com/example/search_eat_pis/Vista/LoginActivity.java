bfbbc2e396dc092d804974f84004f55513bd4419
CarlosGoDo <cgomezdo22@alumnes.ub.edu>
2022-04-25 22:29:20 +0200

    card 4

--- app/src/main/java/com/example/search_eat_pis/Vista/LoginActivity.java
+++ app/src/main/java/com/example/search_eat_pis/Vista/LoginActivity.java
@@ -1,13 +1,16 @@
-package com.example.search_eat_pis;
+package com.example.search_eat_pis.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.search_eat_pis.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/example/search_eat_pis/Vista/RegisterActivity.java
+++ app/src/main/java/com/example/search_eat_pis/Vista/RegisterActivity.java
@@ -1,14 +1,15 @@
-package com.example.search_eat_pis;
+package com.example.search_eat_pis.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.search_eat_pis.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
