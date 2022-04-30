a84199a8aafe65e08d1200752270ee73b55b1928
CarlosGoDo <cgomezdo22@alumnes.ub.edu>
2022-04-30 17:40:03 +0200

    Menu principal

--- app/src/main/java/com/example/search_eat_pis/Vista/MainMenu_Navegacion.java
+++ app/src/main/java/com/example/search_eat_pis/Vista/MainMenu_Navegacion.java
@@ -3,7 +3,6 @@ package com.example.search_eat_pis.Vista;
 import com.example.search_eat_pis.Controller.DatabaseAdapter;
 
 import android.Manifest;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -15,14 +14,11 @@ import com.example.search_eat_pis.Model.Coordenada;
 import com.example.search_eat_pis.Model.Local;
 import com.example.search_eat_pis.Model.Sector;
 import com.example.search_eat_pis.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -43,21 +39,11 @@ public class MainMenu_Navegacion extends AppCompatActivity implements DatabaseAd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainMenuNavegacionBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main_menu_navegacion);
         ultima_ubicación();
         initialize_elements();
         init();
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_menu_navegacion);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
     public void init(){
@@ -75,8 +61,8 @@ public class MainMenu_Navegacion extends AppCompatActivity implements DatabaseAd
     */
     public void initialize_elements(){
 
-        elements.clear(); //limpiamos toda la lista
-        String newString;
+        elements = new ArrayList<>(); //limpiamos toda la lista
+
         if(getIntent().getExtras()==null){
             elements = null;
         }
@@ -140,7 +126,6 @@ public class MainMenu_Navegacion extends AppCompatActivity implements DatabaseAd
 
     @Override
     public void setToast(String s) {
-
 
 
     }
---
+++ app/src/main/java/com/example/search_eat_pis/Vista/Menu_PrincipalActivity.java
@@ -0,0 +1,88 @@
+package com.example.search_eat_pis.Vista;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.search_eat_pis.R;
+import com.ramotion.circlemenu.CircleMenuView;
+
+public class Menu_PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+
+        final CircleMenuView circleMenu = findViewById(R.id.circleMenu);
+        circleMenu.setEventListener(new CircleMenuView.EventListener(){
+
+            @Override
+            public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
+                //do something...
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int buttonIndex) {
+                super.onButtonClickAnimationStart(view, buttonIndex);
+                switch(buttonIndex){
+                    case 0:
+                        Toast.makeText(Menu_PrincipalActivity.this, "Botón de perfil ", Toast.LENGTH_SHORT).show();
+                        perfil(view);
+                        break;
+                    case 1:
+                        Toast.makeText(Menu_PrincipalActivity.this, "Botón de restaurantes ", Toast.LENGTH_SHORT).show();
+                        restaurantes(view);
+                        break;
+                    case 2:
+                        Toast.makeText(Menu_PrincipalActivity.this, "Botón de cafeterias ", Toast.LENGTH_SHORT).show();
+                        cafeteria(view);
+                        break;
+                    case 3:
+                        Toast.makeText(Menu_PrincipalActivity.this, "Botón de bares ", Toast.LENGTH_SHORT).show();
+                        bares(view);
+                        break;
+                    case 4:
+                        Toast.makeText(Menu_PrincipalActivity.this, "acerca del programa ", Toast.LENGTH_SHORT).show();
+                        informacion(view);
+                        break;
+
+
+                }
+            }
+        });
+
+
+
+
+    }
+    public void perfil(View view){
+        Intent perfil = new Intent(this, Perfil.class);
+        startActivity(perfil);
+    }
+
+    public void restaurantes(View view){
+        Intent rest = new Intent(this, MainMenu_Navegacion.class);
+        rest.putExtra("boton","restaurante");
+        startActivity(rest);
+    }
+    public void cafeteria (View view){
+        Intent cafe = new Intent(this, MainMenu_Navegacion.class);
+        cafe.putExtra("boton","cafe");
+        startActivity(cafe);
+    }
+    public void bares(View view){
+        Intent bar = new Intent(this, MainMenu_Navegacion.class);
+        bar.putExtra("boton","bares");
+        startActivity(bar);
+    }
+    public void informacion(View view){
+        Intent info = new Intent(this, Informacion.class);
+        startActivity(info);
+    }
+}
