8bc5bfdbfe86597ff67a3f61ea18ddc9ba4b2575
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2022-04-30 17:26:16 +0200

    DataBase mod

--- app/src/main/java/com/example/search_eat_pis/Vista/LoginActivity.java
+++ app/src/main/java/com/example/search_eat_pis/Vista/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.search_eat_pis.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.search_eat_pis.R;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,5 +31,38 @@ public class LoginActivity extends AppCompatActivity {
     public void Register(View view){
         Intent login_register = new Intent(this, RegisterActivity.class);
         startActivity(login_register);
+    }
+
+    public void setLiveDataObservers() {
+        //Subscribe the activity to the observable
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        viewModel.setCoordenada(coordenada);
+
+        final Observer<Sector> observerSector = new Observer<Sector>() {
+            @Override
+            public void onChanged(Sector s) {
+                viewModel.iniLocal("restaurantes");
+            }
+        };
+
+        final Observer<ArrayList<Local>> observer = new Observer<ArrayList<Local>>() {
+            @Override
+            public void onChanged(ArrayList<Local> l) {
+                CustomAdapter newAdapter = new CustomAdapter(parentContext, l, (CustomAdapter.playerInterface) mActivity);
+                mRecyclerView.swapAdapter(newAdapter, false);
+                newAdapter.notifyDataSetChanged();
+            }
+        };
+        final Observer<String> observerToast = new Observer<String>() {
+            @Override
+            public void onChanged(String t) {
+                Toast.makeText(parentContext, t, Toast.LENGTH_SHORT).show();
+            }
+        };
+        viewModel.getSector().observe(this,observerSector);
+        viewModel.getLocales().observe(this, observer);
+        viewModel.getToast().observe(this, observerToast);
+
+        viewModel.iniSector();
     }
 }
