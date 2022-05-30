b089bf16dc83bf2ccf54016caee3237ea470aee6
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2022-05-30 22:09:47 +0200

    List Adapter Reserva conexión botón eliminar reserva

--- app/src/main/java/com/example/search_eat_pis/Vista/ListAdapter_Reserva.java
+++ app/src/main/java/com/example/search_eat_pis/Vista/ListAdapter_Reserva.java
@@ -1,15 +1,19 @@
 package com.example.search_eat_pis.Vista;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.search_eat_pis.Model.Reserva;
+import com.example.search_eat_pis.Model.Usuario;
 import com.example.search_eat_pis.R;
 
 import java.util.List;
@@ -35,21 +39,35 @@ public class ListAdapter_Reserva extends RecyclerView.Adapter<ListAdapter_Reserv
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ListAdapter_Reserva.ViewHolder_reseva holder, int position) {
+    public void onBindViewHolder(@NonNull ListAdapter_Reserva.ViewHolder_reseva holder, @SuppressLint("RecyclerView") final int position) {
         holder.binData(mData.get(position));
+        holder.getEliminar().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String reservaID = mData.get(position).getId();
+                Usuario.usuario_actual.deleteReserva(reservaID);
+                mData.remove(position);
+            }
+        });
     }
 
     public void setItems(List<Reserva> element){this.mData = element;}
 
     public class ViewHolder_reseva extends RecyclerView.ViewHolder{
-        TextView nombre, fecha,num_pers;
+        private TextView nombre, fecha,num_pers;
+        private ImageButton eliminar;
         String aux;
+
+        public ImageButton getEliminar() {
+            return eliminar;
+        }
 
         public ViewHolder_reseva(View itemView) {
             super(itemView);
             nombre = itemView.findViewById(R.id.nom_rest);
             fecha = itemView.findViewById(R.id.fecha_reserva);
             num_pers = itemView.findViewById(R.id.num_pers);
+            eliminar = itemView.findViewById(R.id.boton_eliminar_reserva);
         }
 
         void binData(final Reserva item){
